63566887030ee85c170dd6e6f5bca0d0c96151e8
beyza pelit <beyzapelitt@gmail.com>
2025-05-14 20:15:53 +0300

    update

--- bitirmeprojesi/settings.py
+++ bitirmeprojesi/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta  # JWT Token Süresi İçin
 import os  # dosyanın en üstüne, pathlib ile birlikte
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Mevcut satırın altına ekle:
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
@@ -20,8 +22,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # deployment için
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
